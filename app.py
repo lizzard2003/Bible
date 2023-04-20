93537e40a1ae186badbe9835be5b22c2eedcaa81
Liz DeLaRosa <59484240+lizzard2003@users.noreply.github.com>
2023-04-19 22:52:35 -0500

    committ5

--- app.py
+++ app.py
@@ -1,50 +1,3 @@
-<<<<<<< HEAD
-from os import getenv
-from dotenv import find_dotenv, load_dotenv
-import flask
-from flask import Flask
-
-import requests
-import random
-
-load_dotenv (find_dotenv())
-
-
-app = Flask(__name__)
-api= "f89bb43fa16f2aad9148c3e7c5d7c31a"
-POSSIBLE_BIBLES = [55, 10368]
-
-
-app.secret_key= "secret"
-
-BIBLE_URL= "https://api.scripture.api.bible/v1/bibles"
-
-app.route('/main')
-def main():
-    random_verse= random.choice(POSSIBLE_BIBLES)
-    response =requests.get(BIBLE_URL+(str(random_verse)+"?"),
-    params={
-        
-        "api_key" : BIBLE_API
-    })
-    response.raise_for_status()
-    bible_data={}
-    id=response.json()["id"]
-    name=response.json()["name"]
-    type=response.json()["type"]
-    
-
-    bible_data= {}
-    bible_data["id"]=id
-    
-    bible_data["name"] = name
-    bible_data["type"]= type
-   
-
-    print (bible_data)
-    return flask.jsonify(bible_data)
-app.run(debug=True)
-=======
 #jazelle and Lizdelarosa
 #login and sing up created..
 #still need API, database, ..
@@ -106,4 +59,4 @@ def logout():
 #logout option after logged on on dashboard page
     session.pop('username', None)
     return redirect('/login')
->>>>>>> origin/main
+
