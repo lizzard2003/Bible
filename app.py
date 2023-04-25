7890f89cb21df9251cf8ba3bed2c530e9f0d4262
Liz DeLaRosa <59484240+lizzard2003@users.noreply.github.com>
2023-04-24 23:32:15 -0500

    changes

--- app.py
+++ app.py
@@ -9,12 +9,12 @@ from flask import Flask, jsonify, render_template, request, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 import requests
 
+
 load_dotenv(find_dotenv())
 app = Flask(__name__)
 app.secret_key = "my_secret_key"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comments.db'
 db = SQLAlchemy(app)
-
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False)
@@ -24,35 +24,23 @@ class User(db.Model):
  
 users = {}
 
-
-#class User(db.Model):
-   # id = db.Column(db.Integer, primary_key=True)
-   # username = db.Column(db.String(80), nullable=False)
-    #message = db.Column(db.String(150), nullable=False)
-    #likes = db.Column(db.Integer, default=0)
-    #dislikes = db.Column(db.Integer, default=0)
-
-
-
 @app.route('/')
 def home():
     return redirect('/login')
-
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-
-        if User.query.filter_by(username=username).first():
+#here is so the user can make their OWN user name y password 
+#sign up html the only affected from this update!
+        if username in users:
             error = "Oops username already exists.."
             return render_template('signup.html', error=error)
-
-        new_user = User(username=username, password=password)
-        db.session.add(new_user)
-        db.session.commit()
-
+        
+        users[username] = password
+    
         session['username'] = username
         return redirect('/dashboard')
     else:
@@ -64,63 +52,29 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-
-        user = user.query.filter_by(username=username).first()
-
-        if not User or user.password != password:
+#wrong pass word or email.. retry 
+        if username not in users or users[username] != password:
             error = "Sorry, Incorrect username or password."
             return render_template('login.html', error=error)
-
         session['username'] = username
-        return redirect('/form')
+        return redirect('/dashboard')
     else:
         return render_template('login.html')
 
-
 @app.route('/dashboard')
 def dashboard():
+#render bk to dashboard..
     if 'username' in session:
-        comments = Comment.query.all()
-        return render_template('dashboard.html', username=session['username'], comments=comments)
+        return render_template('dashboard.html', username=session['username'])
     else:
         return redirect('/login')
-
-
-@app.route('/add_comment', methods=['POST'])
-def add_comment():
-    if 'username' in session:
-        new_comment = Comment(username=session['username'], message=request.form['message'])
-        db.session.add(new_comment)
-        db.session.commit()
-        return redirect('/dashboard')
-    else:
-        return redirect('/login')
-
-
-@app.route('/like_comment', methods=['POST'])
-def like_comment():
-    if 'username' in session:
-        comment_id = request.form['id']
-        action = request.form['action']
-
-        comment = Comment.query.get(comment_id)
-
-        if action == 'like':
-            comment.likes += 1
-        else:
-            comment.dislikes += 1
-
-        db.session.commit()
-
-        return redirect('/dashboard')
-    else:
-        return redirect('/login')
-
 
 @app.route('/logout')
 def logout():
+#logout option after logged on on dashboard page
     session.pop('username', None)
     return redirect('/login')
+
 
 @app.route("/search")
 def search():
@@ -129,8 +83,8 @@ def search():
         return jsonify(error="Missing search query")
     each_entry.clear()
     results = search_scripture(query)
-
-
+    
+    
     return render_template("results.html", each_entry=each_entry, query=query)
 @app.route("/form")
 def form():
@@ -146,10 +100,11 @@ def search_scripture(query):
     bible_data = response_json['data']['verses']
     #each_entry = [] # create list that has a 3 Id bible and so on 
     for i in range(10):
-
+        
         bibleId = bible_data[i]['bookId']
         chapterId = bible_data[i]['chapterId']
         text = bible_data[i]['text']
         each_entry.append({'bibleId': bibleId, 'chapterId': chapterId, 'text': text})
 
-app.run(debug=True, port=5003)
+if __name__ == "__main__":
+    app.run(debug=True, port=5003)
